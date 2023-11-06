20d538dfc64b44d48a956282bb00bc1e311f9ec8
yamilms <yamilosirismelian.santos@volenday.com>
2023-11-06 17:20:45 +0000

    random animations and pass information to another page

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Settings extends StatelessWidget {
@@ -9,34 +11,115 @@ class Settings extends StatelessWidget {
       appBar: AppBar(
         title: const Text('First Route'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color>(
-                Theme.of(context).colorScheme.secondary),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              const AnimatedContainerApp(),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                      Theme.of(context).colorScheme.secondary),
+                ),
+                child: Text(
+                  'Open route',
+                  style: TextStyle(
+                    color: Theme.of(context)
+                        .colorScheme
+                        .onSecondary, // This line applies the onSecondary color to the text
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SecondRoute(
+                            text: "Text passed from other page")),
+                  );
+                },
+              ),
+            ],
           ),
-          child: Text(
-            'Open route',
-            style: TextStyle(
-              color: Theme.of(context)
-                  .colorScheme
-                  .onSecondary, // This line applies the onSecondary color to the text
-            ),
-          ),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const SecondRoute()),
-            );
-          },
         ),
       ),
     );
   }
 }
 
+class AnimatedContainerApp extends StatefulWidget {
+  const AnimatedContainerApp({super.key});
+
+  @override
+  State<AnimatedContainerApp> createState() => _AnimatedContainerAppState();
+}
+
+class _AnimatedContainerAppState extends State<AnimatedContainerApp> {
+  // Define the various properties with default values. Update these properties
+  // when the user taps a FloatingActionButton.
+  double _width = 50;
+  double _height = 50;
+  Color _color = Colors.green;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          AnimatedContainer(
+            // Use the properties stored in the State class.
+            width: _width,
+            height: _height,
+            decoration: BoxDecoration(
+              color: _color,
+              borderRadius: _borderRadius,
+            ),
+            // Define how long the animation should take.
+            duration: const Duration(seconds: 1),
+            // Provide an optional curve to make the animation feel smoother.
+            curve: Curves.fastOutSlowIn,
+          ),
+          ElevatedButton(
+            // When the user taps the button
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                    Theme.of(context).colorScheme.secondary)),
+            onPressed: () {
+              // Use setState to rebuild the widget with new values.
+              setState(() {
+                // Create a random number generator.
+                final random = Random();
+
+                // Generate a random width and height.
+                _width = random.nextInt(300).toDouble();
+                _height = random.nextInt(300).toDouble();
+
+                // Generate a random color.
+                _color = Color.fromRGBO(
+                  random.nextInt(256),
+                  random.nextInt(256),
+                  random.nextInt(256),
+                  1,
+                );
+
+                // Generate a random border radius.
+                _borderRadius =
+                    BorderRadius.circular(random.nextInt(100).toDouble());
+              });
+            },
+            child: Icon(Icons.play_arrow,
+                color: Theme.of(context).colorScheme.onPrimaryContainer),
+          )
+        ],
+      ),
+    );
+  }
+}
+
 class SecondRoute extends StatelessWidget {
-  const SecondRoute({super.key});
+  const SecondRoute({Key? key, required this.text}) : super(key: key);
+  final String text;
 
   @override
   Widget build(BuildContext context) {
@@ -45,18 +128,25 @@ class SecondRoute extends StatelessWidget {
         title: const Text('Second Route'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all<Color>(
-                Theme.of(context).colorScheme.secondary),
-          ),
-          child: Text(
-            'Go back!',
-            style: TextStyle(color: Theme.of(context).colorScheme.onSecondary),
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(text),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                    Theme.of(context).colorScheme.secondary),
+              ),
+              child: Text(
+                'Go back!',
+                style:
+                    TextStyle(color: Theme.of(context).colorScheme.onSecondary),
+              ),
+            ),
+          ],
         ),
       ),
     );
